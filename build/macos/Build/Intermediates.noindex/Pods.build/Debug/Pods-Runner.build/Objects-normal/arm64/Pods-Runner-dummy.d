0d72c445653d199b59b87118ebbbc566dd8b439c
jason218 <jw122322@gmail.com>
2023-03-11 13:37:31 +0800

    update gitignore

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/justinding/Documents/Front\ End\ Learning/Dart_Flutter/flutter_application_1/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/justinding/Documents/Front\ End\ Learning/Dart_Flutter/flutter_application_1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
